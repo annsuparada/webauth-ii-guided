e6e14e927795defe1f6cff40e8f57a3219cf94d7
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-17 11:47:52 -0500

    add cokkie

--- api/server.js
+++ api/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'chocochip', 
+    secret:'keep it secrect',
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+    },
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: true,
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
